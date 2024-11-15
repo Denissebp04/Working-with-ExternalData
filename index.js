3428d918cb533c5c59562b7babeff37ed983edf7
Denisse Belliard <153006924+Denissebp04@users.noreply.github.com>
2024-11-15 15:50:59 -0500

    updating js

--- index.js
+++ index.js
@@ -15,14 +15,7 @@ const BASE_URL = 'https://api.thecatapi.com/v1/'
 // Step 0: Store your API key here for reference and easy access.
 const API_KEY = "live_4gKia4a6SuvTXm0jl2FkS8lp4AIcjVnfW1eBBie3CAiyRunN1XzzmZCkeUOravWl";
 
-/**
- * 1. Create an async function "initialLoad" that does the following:
- * - Retrieve a list of breeds from the cat API using fetch().
- * - Create new <options> for each of these breeds, and append them to breedSelect.
- *  - Each option should have a value attribute equal to the id of the breed.
- *  - Each option should display text equal to the name of the breed.
- * This function should execute immediately.
- */
+//Part 1
 
 let initialLoad = async function (){
     await axios.get(`${BASE_URL}breeds`).then(res => {
@@ -41,20 +34,8 @@ let initialLoad = async function (){
 initialLoad()
 
 
-/**
- * 2. Create an event handler for breedSelect that does the following:
- * - Retrieve information on the selected breed from the cat API using fetch().
- *  - Make sure your request is receiving multiple array items!
- *  - Check the API documentation if you're only getting a single object.
- * - For each object in the response array, create a new element for the carousel.
- *  - Append each of these new elements to the carousel.
- * - Use the other data you have been given to create an informational section within the infoDump element.
- *  - Be creative with how you create DOM elements and HTML.
- *  - Feel free to edit index.html and styles.css to suit your needs, but be careful!
- *  - Remember that functionality comes first, but user experience and design are important.
- * - Each new selection should clear, re-populate, and restart the Carousel.
- * - Add a call to this function to the end of your initialLoad function above to create the initial carousel.
- */
+//Part2
+
 // await axios.get(`${BASE_URL}/breeds/${breedInfo}`
 // const carousel = document.getElementsByClassName('carousel')
 
@@ -123,33 +104,7 @@ async function gallery(images, favorites){
 
 
 
-
-
-
-
-
-    
-
-
-/**
- * 3. Fork your own sandbox, creating a new one named "JavaScript Axios Lab."
- */
-/**
- * 4. Change all of your fetch() functions to axios!
- * - axios has already been imported for you within index.js.
- * - If you've done everything correctly up to this point, this should be simple.
- * - If it is not simple, take a moment to re-evaluate your original code.
- * - Hint: Axios has the ability to set default headers. Use this to your advantage
- *   by setting a default header with your API key so that you do not have to
- *   send it manually with all of your requests! You can also set a default base URL!
- */
-/**
- * 5. Add axios interceptors to log the time between request and response to the console.
- * - Hint: you already have access to code that does this!
- * - Add a console.log statement to indicate when requests begin.
- * - As an added challenge, try to do this on your own without referencing the lesson material.
- */
-
+//Part 3, 4, and 5
 axios.interceptors.request.use(function (config) {
     progressBar.style.width = "0%"
     config.metadata = { startTime: new Date()}
@@ -173,37 +128,13 @@ axios.interceptors.request.use(function (config) {
 
 
 
-/**
- * 6. Next, we'll create a progress bar to indicate the request is in progress.
- * - The progressBar element has already been created for you.
- *  - You need only to modify its "width" style property to align with the request progress.
- * - In your request interceptor, set the width of the progressBar element to 0%.
- *  - This is to reset the progress with each request.
- * - Research the axios onDownloadProgress config option.
- * - Create a function "updateProgress" that receives a ProgressEvent object.
- *  - Pass this function to the axios onDownloadProgress config option in your event handler.
- * - console.log your ProgressEvent object within updateProgess, and familiarize yourself with its structure.
- *  - Update the progress of the request using the properties you are given.
- * - Note that we are not downloading a lot of data, so onDownloadProgress will likely only fire
- *   once or twice per request to this API. This is still a concept worth familiarizing yourself
- *   with for future projects.
- */
+//Part 6
 
 function updateProgress(ProgressEvent){
     console.log(ProgressEvent)
 
     progressBar.style.width = "100%"
 }
-
-
-
-/**
- * 7. As a final element of progress indication, add the following to your axios interceptors:
- * - In your request interceptor, set the body element's cursor style to "progress."
- * - In your response interceptor, remove the progress cursor style from the body element.
- */
-
-
 
 
 
@@ -220,6 +151,9 @@ function updateProgress(ProgressEvent){
  */
 export async function favourite(imgId) {
   // your code here
+
+
+
 }
 
 /**
@@ -231,6 +165,7 @@ export async function favourite(imgId) {
  *    If that isn't in its own function, maybe it should be so you don't have to
  *    repeat yourself in this section.
  */
+
 
 /**
  * 10. Test your site, thoroughly!
